6d97ec46bbec3f31c652c0208e3393d7aa2bee71
Dmitry Shmidt <dimitrysh@google.com>
2013-04-24 11:22:01 -0700

    wpa_supplicant_lib: Notify wpa_supplicant about channels after band change
    Change-Id: Iff80c43e9d8bdd082ea1e04783778f173d0c79b4
    Signed-off-by: Dmitry Shmidt <dimitrysh@google.com>

--- bcmdhd/wpa_supplicant_8_lib/driver_cmd_nl80211.c
+++ bcmdhd/wpa_supplicant_8_lib/driver_cmd_nl80211.c
@@ -79,7 +79,8 @@ int wpa_driver_nl80211_driver_cmd(void *priv, char *cmd, char *buf,
 			    (os_strcasecmp(cmd, "RSSI") == 0) ||
 			    (os_strcasecmp(cmd, "GETBAND") == 0) )
 				ret = strlen(buf);
-			else if (os_strncasecmp(cmd, "COUNTRY", 7) == 0)
+			else if ((os_strncasecmp(cmd, "COUNTRY", 7) == 0) ||
+				 (os_strncasecmp(cmd, "SETBAND", 7) == 0))
 				wpa_supplicant_event(drv->ctx,
 					EVENT_CHANNEL_LIST_CHANGED, NULL);
 			wpa_printf(MSG_DEBUG, "%s %s len = %d, %d", __func__, buf, ret, strlen(buf));
