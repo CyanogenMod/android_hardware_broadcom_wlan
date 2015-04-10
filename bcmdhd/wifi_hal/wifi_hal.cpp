026c12a13d5b8736aaff4786e0f691531334fcba
Pierre Vandwalle <vandwalle@google.com>
2015-04-10 14:20:13 -0700

    changes to support wifi_logger.h API changes
    Change-Id: I20b7d10e21a69817e9ea9d0b444e7976784317ef

--- bcmdhd/wifi_hal/wifi_hal.cpp
+++ bcmdhd/wifi_hal/wifi_hal.cpp
@@ -798,8 +798,7 @@ wifi_error wifi_set_nodfs_flag(wifi_interface_handle handle, u32 nodfs)
 }
 
 wifi_error wifi_start_logging(wifi_interface_handle iface, u32 verbose_level, u32 flags,
-        u32 max_interval_sec, u32 min_data_size, u8 *buffer_name,
-        wifi_ring_buffer_data_handler handler) {
+        u32 max_interval_sec, u32 min_data_size, char *buffer_name) {
             return WIFI_ERROR_NOT_SUPPORTED;
 }
 
